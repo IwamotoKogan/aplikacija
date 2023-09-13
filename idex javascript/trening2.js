be213e9bd90c73cdcbf807d59bceae229de24a09
IwamotoKogan <141932936+IwamotoKogan@users.noreply.github.com>
2023-09-13 17:34:57 +0200

    Update trening2.js

--- idex javascript/trening2.js
+++ idex javascript/trening2.js
@@ -1,115 +1,46 @@
  // Funkcija za izračunavanje cene na osnovu dimenzija
- const dezen1Price = 0;   // Osnovni dezen
+const dezen1Price = 0;   // Osnovni dezen
 const dezen2Price = 50;  // Crni kamen
 const dezen3Price = 70;  // Beli mermer
 
-        function calculatePrice(height, width, depth) {
-            const basePrice = 1000;
-            const increment = 20;
-            const limit = 5;
+function calculatePrice(height, width, depth) {
+    const basePrice = 1000;
+    const increment = 20;
+    const limit = 5;
 
-            const heightDifference = height - 90;
-            const widthDifference = width - 100;
-            const depthDifference = depth - 30;
+    const heightDifference = height - 90;
+    const widthDifference = width - 100;
+    const depthDifference = depth - 30;
 
-            const heightPrice = heightDifference >= 0 ? Math.floor(heightDifference / limit) * increment : 0;
-            const widthPrice = widthDifference >= 0 ? Math.floor(widthDifference / limit) * increment : 0;
-            const depthPrice = depthDifference >= 0 ? Math.floor(depthDifference / limit) * increment : 0;
+    const heightPrice = heightDifference >= 0 ? Math.floor(heightDifference / limit) * increment : 0;
+    const widthPrice = widthDifference >= 0 ? Math.floor(widthDifference / limit) * increment : 0;
+    const depthPrice = depthDifference >= 0 ? Math.floor(depthDifference / limit) * increment : 0;
 
-            const totalPrice = basePrice + heightPrice + widthPrice + depthPrice;
-            return totalPrice;
-        }
+    const totalPrice = basePrice + heightPrice + widthPrice + depthPrice;
+    return totalPrice;
+}
 
-        // Funkcija koja proverava da li su unete vrednosti numeričkog tipa
-        function isValidNumber(value) {
-            return !isNaN(value);
-        }
+// Funkcija koja proverava da li su unete vrednosti numeričkog tipa
+function isValidNumber(value) {
+    return !isNaN(value);
+}
 
-        // Funkcija koja proverava da li su unete dimenzije unutar dozvoljenog opsega
-        function isValidDimensions(height, width, depth) {
-            const minHeight = 90;
-            const maxHeight = 200;
-            const minWidth = 100;
-            const maxWidth = 250;
-            const minDepth = 30;
-            const maxDepth = 60;
+// Funkcija koja proverava da li su unete dimenzije unutar dozvoljenog opsega
+function isValidDimensions(height, width, depth) {
+    const minHeight = 90;
+    const maxHeight = 200;
+    const minWidth = 100;
+    const maxWidth = 250;
+    const minDepth = 30;
+    const maxDepth = 60;
 
-            return height >= minHeight && height <= maxHeight &&
-                width >= minWidth && width <= maxWidth &&
-                depth >= minDepth && depth <= maxDepth;
-        }
+    return height >= minHeight && height <= maxHeight &&
+        width >= minWidth && width <= maxWidth &&
+        depth >= minDepth && depth <= maxDepth;
+}
 
-        // Funkcija koja se poziva prilikom klika na dugme za izračunavanje cene
-        function calculate() {
-            const heightInput = document.getElementById('height');
-            const widthInput = document.getElementById('width');
-            const depthInput = document.getElementById('depth');
-
-            const height = parseInt(heightInput.value);
-            const width = parseInt(widthInput.value);
-            const depth = parseInt(depthInput.value);
-
-            if (!isValidNumber(height) || !isValidNumber(width) || !isValidNumber(depth)) {
-                document.getElementById('price').innerText = "Niste uneli validne podatke";
-                return;
-            }
-
-            if (!isValidDimensions(height, width, depth)) {
-                document.getElementById('price').innerText = "Dimenzije koje ste uneli su izvan dozvoljenog opsega";
-                return;
-            }
-
-
-
-
-
-
-
-            /*******dodato***** */
-            const dezenSelect = document.getElementById('selected-pattern-title');
-    const selectedDezen = dezenSelect.textContent;
-
-    let dezenPrice = 0;
-    if (selectedDezen === "Crni kamen") {
-        dezenPrice = dezen2Price;
-    } else if (selectedDezen === "Beli mermer") {
-        dezenPrice = dezen3Price;
-    }
-            /*******dodato***** */
-
-
-
-
-
-
-            const totalPrice = calculatePrice(height, width, depth) + dezenPrice;
-
-            document.getElementById('price').innerText = `Cena: ${totalPrice} evra`;
-
-            /*front*/
-            const recommendedFrontDimensions = calculateRecommendedFrontDimensions(height, width, depth);
-
-    // Prikaz preporučenog fronta ispod cene
-    const recommendedFront = document.getElementById('recommended-front');
-    recommendedFront.innerHTML = `Dimenzije fronta za ovaj element visina ${recommendedFrontDimensions.recommendedHeight}cm i širina ${recommendedFrontDimensions.recommendedWidth}cm`;
-            /*front*/
-        }
-
-        document.getElementById('calculate-btn').addEventListener('click', calculate);
-
-        /************Dodati elementi za skladištenje podataka************ */
-        // ... (ostatak JavaScript koda za kalkulator)
-
-// Kreiramo objekat za čuvanje podataka o kuhinji
-let kuhinjaData = {
-    height: 0,
-    width: 0,
-    depth: 0,
-    price: 0
-};
-
-
-    function addToCart() {
+// Funkcija koja se poziva prilikom klika na dugme za izračunavanje cene
+function calculate() {
     const heightInput = document.getElementById('height');
     const widthInput = document.getElementById('width');
     const depthInput = document.getElementById('depth');
@@ -128,42 +59,73 @@ let kuhinjaData = {
         return;
     }
 
-   
+    const totalPrice = calculatePrice(height, width, depth);
 
-    //const totalPrice = calculatePrice(height, width, depth);
+    document.getElementById('price').innerText = `Cena: ${totalPrice} evra`;
 
-    // Popunimo podatke o kuhinji
+    // Izračunajte preporučene dimenzije fronta
+    const recommendedFrontDimensions = calculateRecommendedFrontDimensions(height, width, depth);
+
+    // Prikaz preporučenog fronta ispod cene
+    const recommendedFront = document.getElementById('recommended-front');
+    recommendedFront.innerHTML = `Dimenzije fronta za ovaj element visina ${recommendedFrontDimensions.recommendedHeight}cm i širina ${recommendedFrontDimensions.recommendedWidth}cm`;
+}
+
+document.getElementById('calculate-btn').addEventListener('click', calculate);
+
+// Kreiramo objekat za čuvanje podataka o kuhinji
+let kuhinjaData = {
+    height: 0,
+    width: 0,
+    depth: 0,
+    price: 0
+};
+
+function addToCart() {
+    const heightInput = document.getElementById('height');
+    const widthInput = document.getElementById('width');
+    const depthInput = document.getElementById('depth');
+
+    const height = parseInt(heightInput.value);
+    const width = parseInt(widthInput.value);
+    const depth = parseInt(depthInput.value);
+
+    if (!isValidNumber(height) || !isValidNumber(width) || !isValidNumber(depth)) {
+        document.getElementById('price').innerText = "Niste uneli validne podatke";
+        return;
+    }
+
+    if (!isValidDimensions(height, width, depth)) {
+        document.getElementById('price').innerText = "Dimenzije koje ste uneli su izvan dozvoljenog opsega";
+        return;
+    }
+
+    const totalPrice = calculatePrice(height, width, depth);
+
     kuhinjaData.height = height;
     kuhinjaData.width = width;
     kuhinjaData.depth = depth;
     kuhinjaData.price = totalPrice;
 
-    // Ažuriramo brojčanik na korpi
     const cartItems = document.getElementById('cart-items');
     cartItems.innerText = parseInt(cartItems.innerText) + 1;
 
-   // Prikažemo notifikaciju +1 na dugmetu "Korpa"
     const cartButton = document.getElementById('cart-button');
     const notification = document.createElement('div');
     notification.className = 'notification';
     notification.innerText = '+1';
     cartButton.appendChild(notification);
 
-    /*krava*/
     // Izračunajte preporučene dimenzije fronta
     const recommendedFrontDimensions = calculateRecommendedFrontDimensions(height, width, depth);
 
     // Dodajte preporučene dimenzije fronta u objekat kuhinjaData
     kuhinjaData.recommendedFrontDimensions = recommendedFrontDimensions;
 
-    // ... (ostali kod)
-    /**krava */
-
     // Sačuvajmo podatke u localStorage
     localStorage.setItem('kuhinjaData', JSON.stringify(kuhinjaData));
 
-    /***dodato*** */
-     const newItem = {
+    const newItem = {
         height: height,
         width: width,
         depth: depth,
@@ -181,21 +143,11 @@ let kuhinjaData = {
 
     // Redirektuj na stranicu pregled_kuhinja.html
     location.reload();
-    /***dodato*** */
-
-
-
 }
 
-// Dodelimo događaj "onclick" dugmetu "Kupi"
 const kupiBtn = document.getElementById('kupi-btn');
 kupiBtn.addEventListener('click', addToCart);
 
-
-
-         /************KRAJ Dodati elementi za skladištenje podataka************ */
-
-         /*front*/
 // Nakon izračunavanja cene, izračunajte preporučene dimenzije fronta
 function calculateRecommendedFrontDimensions(height, width, depth) {
     const recommendedHeight = height - 0.4; // Smanjite visinu za 4mm
@@ -203,7 +155,3 @@ function calculateRecommendedFrontDimensions(height, width, depth) {
     return { recommendedHeight, recommendedWidth };
 }
 
-// Funkcija koja se poziva prilikom klika na dugme za izračunavanje cene
-
-
-/*front*/

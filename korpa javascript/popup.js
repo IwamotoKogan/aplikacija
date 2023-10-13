6845fa58818b2a53450664e7f72929ccab48e9c1
IwamotoKogan <141932936+IwamotoKogan@users.noreply.github.com>
2023-10-13 17:37:01 +0200

    Update popup.js

--- korpa javascript/popup.js
+++ korpa javascript/popup.js
@@ -240,5 +240,5 @@ confirmOrderButton.addEventListener('click', function () {
     window.location.href = "https://iwamotokogan.github.io/DIPO/thanks.html";
 });
 
-/*nova verzija*/
+/*nova verzija2*/
 

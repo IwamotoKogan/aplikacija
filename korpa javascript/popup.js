ac54dceea19a5f6ba30206abc35c9a66b6bb1122
IwamotoKogan <141932936+IwamotoKogan@users.noreply.github.com>
2023-10-17 19:10:47 +0200

    Update popup.js

--- korpa javascript/popup.js
+++ korpa javascript/popup.js
@@ -25,6 +25,7 @@ if (savedItems.length > 0) {
             <p>Kreiraliiii ste element: ${item.height}cm x ${item.width}cm x ${item.depth}cm, cena vašeg elementa je ${item.price} evra.</p>
             <p>Front: Preporučene dimenzije visina ${recommendedFrontDimensions.recommendedHeight}cm i širina ${recommendedFrontDimensions.recommendedWidth}cm.</p>
             <p>Dezen koji ste izabrali: ${item.dezen}</p>
+            <p>Front: ${item.recommendedFrontDimensions.message}</p>
             <button class="order-button" data-index="${index}">Naruči</button>
 
             <button class="delete-button" data-index="${index}">Izbriši</button>
